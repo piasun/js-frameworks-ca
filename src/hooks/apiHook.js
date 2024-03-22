d99a7f3ec652fa51136c6f93911a63edd3c454ec
piasun <77275966+piasun@users.noreply.github.com>
2024-03-22 14:43:07 +0100

    created apihook

---
+++ src/hooks/apiHook.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+
+const useApi = (url) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, [url]);
+
+  return {products, loading, error};
+
+}
+
+export default useApi;
